9f148108b6e783a203f6853eb3fe9b82fa60ba61
Steph Tomlin <stephtomlin@Stephs-MacBook-Air.local>
2020-07-22 15:19:10 -0500

    modified script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 // Assignment Code
 //var generateBtn = document.querySelector("#generate");
-let passowrd = "";
+let password = "";
 
+alert("To begin, press Generate Password");
+
+document.getElementById("generate").addEventListener("click", beginPrompts);
+
+function beginPrompts) {
 //Password Length
 
   var passLength = prompt("How many characters would you like in your password?" );
-if (passLength < "8" && passLength > "128") {
+if (passLength < "8" && passLength > "128" && passLength === null) {
     alert("Password length must be between 8 and 128 characters");
 }  
 
-while (passLength < "8" && passLength> "128") {
+while (passLength < "8" && passLength> "128" && passLength === null) {
     var passLength = prompt("How many characters would you like in your password?");
 }
 //Password Criteria Arrays
@@ -47,47 +52,6 @@ if (confirm("Would you like your password to include symbols?")) {
 } else {
     alert ("Your password will NOT include symbols")
 } 
-//Password Criteria Confirms
-function myFunction() {
-    if (confirm ("Would you like your password to include lowercase letters?" )){
-        
-        alert ("Your password will include lowercase letters");
-        criteriaArray.push (randomLowers);     
-        
-    } else {
-        alert ("Your password will NOT include lowercase letters")
-    }
-function myFunction() {
-    if (confirm ("Would you like your password to include uppercase letters?" )){
-        
-        alert ("Your password will include uppercase letters");
-        criteriaArray.push (randomUppers);     
-        
-    } else {
-        alert ("Your password will NOT include uppercase letters")
-    }
-function myFunction() {
-    if (confirm ("Would you like your password to include numbers?" )){
-         
-        alert ("Your password will include numbers");
-        criteriaArray.push (randomNumbers);     
-        
-    } else {
-        alert ("Your password will NOT include numbers")
-    }
-function myFunction() {
-    if (confirm ("Would you like your password to include symbols?" )){
-         
-        alert ("Your password will include symbols");
-        criteriaArray.push (randomSymbols);     
-        
-    } else {
-        alert ("Your password will NOT include symbols")
-    }
-const criteriaArray = []
-if (criteriaArray === null){
-     return " "; 
-}
 //Click event 
     
 //Create random string and show in box  
