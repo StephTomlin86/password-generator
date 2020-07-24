690176d744441d23e05a3b9e8501f6ee3917f7f0
Steph Tomlin <stephtomlin@Stephs-MacBook-Air.local>
2020-07-23 23:05:24 -0500

    Final commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 alert("To begin press Generate Password");
+
 document.getElementById("generate").addEventListener("click", beginPrompts);
+let criteriaArray = [];
 function beginPrompts() {
   var passLength = prompt(
     "How many characters would you like in your password?"
@@ -13,19 +15,19 @@ function beginPrompts() {
     );
   }
   const randomLowers = "abcdefghijklmnopqrstuvwxyz";
-  var lowers = "abcdefghijklmnopqrstuvwxyz".split("");
-  let criteriaArray = [];
+
+  //var lowers = "abcdefghijklmnopqrstuvwxyz".split("");
   if (confirm("Would you like your password to include lowercase letters?")) {
     alert("Your password will include lowercase letters");
-    criteriaArray.push(lowers);
+    criteriaArray.push(randomLowers);
+    //Here is where I am putting the original string
   } else {
     alert("Your password will NOT include lowercase letters");
   }
   const randomUppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   if (confirm("Would you like your password to include uppercase letters?")) {
     alert("Your password will include uppercase letters");
-    criteriaArray.push(uppers);
+    criteriaArray.push(randomUppers);
   } else {
     alert("Your password will NOT include uppercase letters");
   }
@@ -37,29 +39,29 @@ function beginPrompts() {
     alert("Your password will NOT include numbers");
   }
   const randomSymbols = "!@#$%^&*()";
-  var symbols = "!@#$%^&*()".split("");
   if (confirm("Would you like your password to include symbols?")) {
     alert("Your password will include symbols");
     criteriaArray.push(randomSymbols);
   } else {
     alert("Your password will NOT include symbols");
   }
+  var newCriteriaArray = [criteriaArray.split("")];
 }
-console.log(criteriaArray);
-alert("To see your password press Generate Password");
-document.getElementById("generate").addEventListener("click", genPassword);
+
 function genPassword() {
   let password = "";
-  for (var i = 0; i <= passLength; i++);
-  {
+
+  for (var i = 0; i <= passLength; i++) {
     password =
       password +
-      criteriaArray[Math.floor(Math.random() * criteriaArray.length)];
+      newCriteriaArray[Math.floor(Math.random() * newCriteriaArray.length)];
+
+    // Write password to the #password input
+    function showPassword() {
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
   }
-}
-console.log(password);
-// Write password to the #password input
-function showPassword() {
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  //Calling the function
+  showPassword();
 }
